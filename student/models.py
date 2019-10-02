09a6cd25d0088f8f1bb8e666812295002803794d
unknown <kliannesto@gmail.com>
2019-10-02 13:40:47 +0800

    added urls, models, views and course model

--- student/models.py
+++ student/models.py
@@ -5,10 +5,13 @@ from django.db import models
 class Course(models.Model):
     name= models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class Student(models.Model):
-    student_id = models.IntegerField()
+    student_id = models.IntegerField(unique=True)
     fullname = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     mobileno = models.CharField(max_length=30)
     guardiancontactno = models.CharField(max_length=30)
-    course = models.CharField(max_length=20)
+    course = models.ForeignKey(Course,on_delete = models.CASCADE, null = True, blank=True)
